84f8f05c79a4400288cd629ee7beb1dc84e3df26
fampoo <99708233+fampoo@users.noreply.github.com>
2022-03-08 19:17:57 +0600

    Update problem3.py

--- lab 1/problem3.py
+++ lab 1/problem3.py
@@ -4,5 +4,5 @@ def compound_interest_2019_1_60_063(P,R,T):
 P = float(input("Enter principle amount: "))
 R = float(input("Enter interest amount: "))
 T = float(input("Enter time(years): "))
-com_intreast= compound_interest_2019_1_60_063(P,R,T)
+com_intreast= compound_interest_2019_1_60_064(P,R,T)
 print("compound interest based on the given formula is= ", com_intreast)
